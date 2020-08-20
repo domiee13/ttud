c5c606ab05ef308e759e409f278cdb72e730802d
Domiee13 <dungngocmd@gmail.com>
2020-08-20 16:04:51 +0700

    Update 20/8

--- ex35.cpp
+++ ex35.cpp
@@ -17,15 +17,28 @@ using namespace std;
 const long long mod = 1e9+7;
 long long n,k;
 
-long long Mul(long long a,long long b){
-    if(b==0) return 
+//a*b
+long long Mul(long long a, long long b){
+    if(b==0) return 0;
+    long long tmp = Mul(a,b/2);
+    if(b%2==1) return (tmp*2+a)%mod;
+    return tmp*2%mod;
 }
 
+long long Pow(long long a, long long b){
+    if(b==0) return 1;
+    if(b==1) return a%mod;
+    long long tmp = Pow(a,b/2);
+    long long res = Mul(tmp,tmp);
+    if(b%2==1) return res*a%mod;
+    return res%mod;
+}
 int main(){
     int t;
     cin>>t;
     while(t--){
-
+        cin>>n>>k;
+        cout<<Pow(n,k)<<endl;
     }
     return 0;
 }
